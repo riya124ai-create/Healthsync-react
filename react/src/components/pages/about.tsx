f1ce020fb31aeb1e1546fdf625373ab9d321d1ec
AkankshRakesh <akankshrakesh@gmail.com>
2025-12-10 14:45:54 +0530

    Header addition in about us

--- react/src/components/pages/about.tsx
+++ react/src/components/pages/about.tsx
@@ -1,9 +1,11 @@
+import { Header } from "../header";
+
 export default function About() {
     return (
         <main className="min-h-screen bg-background relative">
-            <div className="relative z-10">
-                <h1>About Page</h1>
-            </div>
+                  <div className="relative z-10">
+                    <Header />
+                  </div>
         </main>
     )
 }
